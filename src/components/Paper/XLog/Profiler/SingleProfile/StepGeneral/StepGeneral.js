df81d0a2f703186acf7d57d2e64656de2f1ac894
beperson <beperson@naver.com>
2018-03-25 03:26:27 +0900

    - 정렬 및 디자인 수정

--- src/components/Paper/XLog/Profiler/SingleProfile/StepGeneral/StepGeneral.js
+++ src/components/Paper/XLog/Profiler/SingleProfile/StepGeneral/StepGeneral.js
@@ -24,8 +24,9 @@ class StepGeneral extends Component {
                 <span className="type">{this.props.type}</span>
                 <span className="start-time"><Moment date={new Date(Number(stepStartTime))} format="YYYY-MM-DD HH:mm:ss.SSS"></Moment></span>
                 <span className="elapsed">{this.props.elapsed !== undefined ? this.props.elapsed + " ms": ""}</span>
-                {this.props.row.step.stepType === "9" && <span className="value">{Number(this.props.row.step.updated) >= 0 && '#' + this.props.row.step.updated}</span>}
-                {this.props.row.step.stepType !== "9" && <span className="value">{!isNaN(this.props.row.step.value) && '#' + this.props.row.step.value}</span>}
+                {this.props.row.step.stepType === "16" && <span className="value">{Number(this.props.row.step.updated) >= 0 && '#' + this.props.row.step.updated}</span>}
+                {this.props.row.step.stepType !== "16" && <span className="value">{!isNaN(this.props.row.step.value) && '#' + this.props.row.step.value}</span>}
+
             </div>)
     }
 }
