758c01a8371d550f06499e77d2a015a2844e2193
gunhlee <gunh.lee@snowcorp.com>
2018-05-14 21:30:44 +0900

    scouter에서 literal 변수가 내부적으로 변수 처리된 부분에 대해 쿼리 바인딩 대응

--- src/components/Paper/XLog/Profiler/SingleProfile/Sql3Step/Sql3Step.js
+++ src/components/Paper/XLog/Profiler/SingleProfile/Sql3Step/Sql3Step.js
@@ -25,14 +25,17 @@ class Sql3Step extends Component {
         let sql = "";
 
         if (this.props.bind) {
-            let params = this.props.row.step.param.split(",");
-            for (let i = 0; i < params.length; i++) {
-                params[i] = "<span class='param'>" + params[i] + "</span>";
+            let params = divideParams(this.props.row.step.param);
+            let {sql: sql0, params: params0} = literalBind(this.props.row.mainValue, params);
+
+            for (let i = 0; i < params0.length; i++) {
+                params0[i] = "<span class='param'>" + params0[i] + "</span>";
             }
-            sql = sqlFormatter.format(this.props.row.mainValue, {
-                params: params,
+            sql = sqlFormatter.format(sql0, {
+                params: params0,
                 indent: "  "
             });
+
         } else {
             sql = sqlFormatter.format(this.props.row.mainValue, {
                 indent: "  "
@@ -49,6 +52,77 @@ class Sql3Step extends Component {
                 <div className="sql-param">[{this.props.row.step.param}]</div>
             </div>)
     }
+}
+
+function literalBind(sql, params) {
+    if(!params) return {sql: sql, params: []};
+    if(!sql) return {sql: sql, params: params};
+
+    const re = /@{\d+}/g;
+
+    let boundSql = "";
+    let pos = 0;
+    let index = 0;
+
+    while(true) {
+        let matched = re.exec(sql);
+        if (matched) {
+            boundSql = boundSql
+                + sql.substring(pos, matched.index)
+                + stripSideChar(params[index], "'");
+
+            pos = matched.index + matched[0].length;
+            index++;
+
+        } else {
+            break;
+        }
+    }
+
+    boundSql = boundSql + sql.substring(pos);
+
+    return {
+        sql: boundSql,
+        params: params.slice(index)
+    }
+
+}
+
+function stripSideChar(str, ch) {
+    if (!str) {
+        return str;
+    }
+    if (str[0] === ch && str[str.length - 1] === ch) {
+        return str.substring(1, str.length -1);
+    }
+
+    return str;
+}
+
+function divideParams(params) {
+    if(!params) return;
+
+    const paramArray = [];
+
+    let start = 0;
+    let isQ = false;
+    let isDQ = false;
+
+    Array.from(params).forEach((ch, i) => {
+        if (ch === ',' && !isQ && !isDQ) {
+            paramArray.push(params.substring(start, i));
+            start = i + 1;
+
+        } else if (ch === "'" && !isDQ) {
+            isQ = !isQ;
+
+        } else if (ch === '"' && !isQ) {
+            isDQ = !isDQ;
+        }
+    });
+    paramArray.push(params.substring(start));
+
+    return paramArray;
 }
 
 export default Sql3Step;
