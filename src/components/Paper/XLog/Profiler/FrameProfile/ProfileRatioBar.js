352f56aa6370f327f7dd62d867aed0eec9951522
voyagerwoo <voyager.woo@gmail.com>
2019-03-10 20:06:35 +0900

    XLog 프로파일 뷰의 상단에 SQL / API Call / OTHERS의 수행 시간 비율을 시각화 하여 표현 #144 - 범례 추가 - 바 내부에 시간 정보 추가

--- src/components/Paper/XLog/Profiler/FrameProfile/ProfileRatioBar.js
+++ src/components/Paper/XLog/Profiler/FrameProfile/ProfileRatioBar.js
@@ -14,29 +14,59 @@ export default class ProfileRatioBar extends Component {
             sqlPercent: 0,
             apiPercent: 0,
             otherPercent: 100,
+            cpu: 0,
+            sql: 0,
+            api: 0,
+            elapsed: 0
         };
+    }
+
+    ratioStyle = (percent) => {
+        if (percent < 0.3) {
+            return {display: 'none'};
+        }
+        
+        return {width: `${percent}%`};
     }
 
     componentDidMount() {
         const {cpu, sql, api, elapsed} = this.props;
-        this.setState({'cpuPercent': calcPercent(cpu, elapsed)});
-        this.setState({'sqlPercent': calcPercent(sql, elapsed)});
-        this.setState({'apiPercent': calcPercent(api, elapsed)});
-        this.setState({'otherPercent': calcPercent(elapsed-cpu-sql-api, elapsed)});
+        const other = (elapsed - cpu - sql - api) > 0 ? (elapsed - cpu - sql - api) : 0;
+
+        this.setState({cpu, sql, api, other});
+        this.setState({'cpuPercent': calcPercent(cpu, elapsed), 
+            'sqlPercent': calcPercent(sql, elapsed), 
+            'apiPercent': calcPercent(api, elapsed), 
+            'otherPercent': calcPercent(other, elapsed)});
     }
 
     render() {
         return (
             <div className="ratio-bar">
-                <div className="ratio-header">
-                    <div> PROFILE STEP</div>
+                <div className="legend-row">
+                    <div className="legend-header">
+                        <div></div>
+                    </div>
+                    <div className="ratio-legend">
+                        <div>
+                            <div className="legend cpu">CPU</div>
+                            <div className="legend sql">SQL</div>
+                            <div className="legend api">API</div>
+                            <div className="legend other">OTHER</div>
+                        </div>
+                    </div>
                 </div>
-                <div className="ratio-body">
-                    <div>
-                        <div className="ratio cpu" style={{width: `${this.state.cpuPercent}%`}}></div>
-                        <div className="ratio sql" style={{width: `${this.state.sqlPercent}%`}}></div>
-                        <div className="ratio api" style={{width: `${this.state.apiPercent}%`}}></div>
-                        <div className="ratio other"></div>
+                <div className="ratio-row">
+                    <div className="ratio-header">
+                        <div> PROFILE STEP</div>
+                    </div>
+                    <div className="ratio-body">
+                        <div>
+                            <div className="ratio cpu" style={this.ratioStyle(this.state.cpuPercent)}>{this.state.cpu}ms</div>
+                            <div className="ratio sql" style={this.ratioStyle(this.state.sqlPercent)}>{this.state.sql}ms</div>
+                            <div className="ratio api" style={this.ratioStyle(this.state.apiPercent)}>{this.state.api}ms</div>
+                            <div className="ratio other" style={this.ratioStyle(this.state.otherPercent)}>{this.state.other}ms</div>
+                        </div>
                     </div>
                 </div>
             </div>);
