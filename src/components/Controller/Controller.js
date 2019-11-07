593af7cf913a6cc999ddf5ee26485266cae01bea
kranian <ysheo@irexnet.co.kr>
2019-11-07 16:20:19 +0900

    :bug: server 변경시 토폴로지 설정이 존재 시 에러가 나는 현상 수정

--- src/components/Controller/Controller.js
+++ src/components/Controller/Controller.js
@@ -270,7 +270,10 @@ class Controller extends Component {
         common.setTargetServerToUrl(this.props, config);
         common.replaceAllLocalSettingsForServerChange(currentServer, this.props, config);
         common.clearAllUrlParamOfPaper(this.props, config);
+        //- server가 바뀌어 쓰므로 해당 설정이 같이 삭제 되어야함
         localStorage.removeItem("selectedObjects");
+        localStorage.removeItem("topologyOptions");
+        localStorage.removeItem("topologyPosition");
         window.location.reload();
 
     };
