31e086479a94782c133467b62b6dd502b0b7f72e
kranian <ysheo@irexnet.co.kr>
2020-05-12 12:19:27 +0900

    active service servier id adding

--- src/components/Paper/ActiveService/ActiveService.js
+++ src/components/Paper/ActiveService/ActiveService.js
@@ -106,7 +106,7 @@ class ActiveService extends Component {
         this.props.setControlVisibility("Loading", true);
         this.props.addRequest();
         const {config, user} = this.props;
-        const _url = `${getHttpProtocol(config)}/scouter/v1/activeService/ofObject/${activeObj.objHash}`;
+        const _url = `${getHttpProtocol(config)}/scouter/v1/activeService/ofObject/${activeObj.objHash}?serverId=${this.props.serverId.server[0].id}`;
         jQuery.ajax({
             method: "GET",
             async: true,
@@ -142,7 +142,7 @@ class ActiveService extends Component {
         this.props.setControlVisibility("Loading", true);
         this.props.addRequest();
         const {config, user} = this.props;
-        const _url = `${getHttpProtocol(config)}/scouter/v1/activeService/thread/${activeThread.threadId}/ofObject/${activeThread.objHash}`;
+        const _url = `${getHttpProtocol(config)}/scouter/v1/activeService/thread/${activeThread.threadId}/ofObject/${activeThread.objHash}?serverId=${this.props.serverId.server[0].id}`;
         jQuery.ajax({
             method: "GET",
             async: true,
@@ -232,7 +232,8 @@ let mapStateToProps = (state) => {
         activeObject: state.target.activeObject,
         config: state.config,
         user: state.user,
-        filterMap: state.target.filterMap
+        filterMap: state.target.filterMap,
+        serverId: state.serverId
     };
 };
 
